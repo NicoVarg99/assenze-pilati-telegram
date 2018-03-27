ed401b30fa5c06f409a340a35bee62fbb4139500
Nicola Salsotto <nicolasalsotto@gmail.com>
2018-03-28 00:33:32 +0200

    Working new bot :)

--- index.js
+++ index.js
@@ -1,216 +1,114 @@
 const https = require('https');
-const cheerio = require('cheerio');
-const cheerioTableparser = require('cheerio-tableparser');
 const fs = require("fs");
 const TelegramBot = require('node-telegram-bot-api');
-var requestedClass = "-";
+const timediff = require('timediff');
 var requestedSubstitute = "-";
-var token =  fs.readFileSync('token', 'utf8').trim();
+const token =  fs.readFileSync('token', 'utf8').trim();
+var assenze; //Data or null
+var errorMessage = "Errore nella richiesta al sito scolastico.\nProva a visitarlo manualmente: https://www.istitutopilati.it/gestione_sostituzioni/slideshow_fermo.php";
 
-function transpose(a) {
-  var w = a.length || 0;
-  var h = a[0] instanceof Array ? a[0].length : 0;
-  if (h === 0 || w === 0) { return []; }
-  var i, j, t = [];
-  for (i = 0; i < h; i++) {
-    t[i] = [];
-    for(j = 0; j < w; j++) {
-      t[i][j] = a[j][i];
-    }
-  }
-
-  return t;
-}
-
-function deleteRow(arr, row) {
-  arr = arr.slice(0); // make copy
-  arr.splice(row - 1, 1);
-  return arr;
-}
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 
 var connectionOptions = {
   hostname: 'www.istitutopilati.it',
   port: '443',
-  path: '/notizie-video-sostituzioni.html',
+  path: '/gestione_sostituzioni/lista.json',
   method: 'GET',
-  headers: { 'Content-Type': 'application/html' },
+  headers: { 'Content-Type': 'application/json' },
   strictSSL: false
 };
 
 function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
-
-function beautifyArray(array, assente, sostituto, orario, classe, note) {
-  var string = "";
-  if (assente)
-  string += "Assente: " + toTitleCase(array[3]) + "\n";
-  if (sostituto)
-    string += "Sostituto: " + toTitleCase(array[0]) + "\n";
-  if (orario)
-    string += "Orario: " + array[1].replace(/\./i, ':').replace(/\./i, ':').replace(/-/i, ' - ') + "\n";
-  if (classe)
-    string += "Classe: " + array[2] + "\n";
-  if (note && array[4])
-    string += "Note: " + array[4] + "\n";
-
-  return string;
-}
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
 
 function msgAssenze(msg, match) {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-  requestedClass = match[1].toUpperCase();
+  var requestedClass = match[1].toUpperCase();
+  var resultingTable = [];
 
-  var response = "";
-  var req = https.request(connectionOptions, function(res) {
-    var totalData = "";
+  if (!assenze) { //If no data is there, send an error message
+    bot.sendMessage(msg.chat.id, errorMessage);
+    return;
+  }
 
-    res.on('data', function (data) { totalData += data; });
+  for (var i in assenze.values) //For each row
+    if (assenze.values[i].Classe == requestedClass) //If curent class = requested class
+      resultingTable.push(assenze.values[i]);
 
-    res.on('end', function () {
+  var numResults = 0;
+  response = "Per " + assenze.data_stringa + " nella classe " + requestedClass + " sono previste le seguenti assenze:\n";
 
-      $ = cheerio.load(totalData);
-      cheerioTableparser($);
-      var table = $(".textContent table").parsetable(false, false, true);
+  for (var i = 0; i < resultingTable.length; i++) { //For each match
+    response += "\nAssente: " + resultingTable[i].Prof_Assente + "\n";
+    response += "Sostituto: " + resultingTable[i].Prof_Sostituto + "\n";
+    response += "Orario: " + resultingTable[i].Orario + "\n";
+    if (resultingTable[i].Note != "")
+      response += "Note: " + resultingTable[i].Note + "\n";
+    numResults++;
+  }
 
-      if (table === undefined || table.length == 0) {
-        console.log("Error");
-        bot.sendMessage(chatId, "Errore nella richiesta al sito scolastico. Prova a visitarlo manualmente: https://www.istitutopilati.it/notizie-video-sostituzioni.html");
-      } else {
-        table = transpose(table);
-        date = table[0][0];
-        righe = table.length;
-        var removed = 0;
-        for (var i = 0; i < righe - removed; i++)
-          if (!table[i][2] || table[i][2] == "CLASSE") {
-            table.splice(i, 1);
-            i--;
-            removed++;
-          }
-        righe = table.length;
-        var resultingTable = [];
+  if (numResults == 0)
+    response = "Nessuna assenza prevista per " + assenze.data_stringa + " nella classe " + requestedClass + ".";
 
-        for (var row in table) //For each row
-          if (table[row][2] == requestedClass) //If curent class = requested class
-            resultingTable.push(table[row]);
-
-        var numResults = 0;
-
-        response = "Per il " + date + " nella classe " + requestedClass + " sono previste le seguenti assenze:\n";
-
-        for (var j = 0; j < resultingTable.length; j++) { //For each column
-          response += "\n" + beautifyArray(resultingTable[j], true, true, true, true, true);
-          numResults++;
-        }
-
-        if (numResults == 0)
-          response = "Nessuna assenza prevista per il " + date + " nella classe " + requestedClass + ".";
-
-        bot.sendMessage(msg.chat.id, response);
-      }
-    });
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-  });
-
-  req.write("");
-  req.end();
-
+  bot.sendMessage(msg.chat.id, response);
 }
 
 function msgSostituto(msg, match) {
-  requestedSubstitute = match[1].toUpperCase();
+  //Searchs by substitute
+  var requestedSubstitute = match[1];
+  var resultingTable = [];
 
-  var response = "";
-  var req = https.request(connectionOptions, function(res) {
-    var totalData = "";
+  if (!assenze) { //If no data is there, send an error message
+    bot.sendMessage(msg.chat.id, errorMessage);
+    return;
+  }
 
-    res.on('data', function (data) { totalData += data; });
+  for (var i in assenze.values) //For each row
+    if (assenze.values[i].Prof_Sostituto == requestedSubstitute) //If curent class = requested class
+      resultingTable.push(assenze.values[i]);
 
-    res.on('end', function () {
+  var numResults = 0;
+  response = "Per " + assenze.data_stringa + " al docente " + requestedSubstitute + " sono assegnate le seguenti sostituzioni:\n";
 
-      $ = cheerio.load(totalData);
-      cheerioTableparser($);
-      var table = $(".textContent table").parsetable(false, false, true);
+  for (var i = 0; i < resultingTable.length; i++) { //For each match
+    response += "\nAssente: " + resultingTable[i].Prof_Assente + "\n";
+    response += "Orario: " + resultingTable[i].Orario + "\n";
+    response += "Classe: " + resultingTable[i].Classe + "\n";
+    if (resultingTable[i].Note != "")
+      response += "Note: " + resultingTable[i].Note + "\n";
+    numResults++;
+  }
 
-      if (table === undefined || table.length == 0) {
-        console.log("Error");
-        bot.sendMessage(chatId, "Errore nella richiesta al sito scolastico. Prova a visitarlo manualmente: https://www.istitutopilati.it/notizie-video-sostituzioni.html");
-      } else {
-        table = transpose(table);
-        date = table[0][0];
-        righe = table.length;
-        var removed = 0;
-        for (var i = 0; i < righe - removed; i++)
-          if (!table[i][0] || table[i][0] == "DOCENTE SOSTITUTO") {
-            table.splice(i, 1);
-            i--;
-            removed++;
-          }
-        righe = table.length;
-        var resultingTable = [];
+  if (numResults == 0)
+     response = "Nessuna assenza assegnata per " + assenze.data_stringa + " al docente " + requestedSubstitute + ".";
 
-        for (var row in table) //For each row
-          if (table[row][0] == requestedSubstitute) //If curent class = requested class
-            resultingTable.push(table[row]);
-
-        var numResults = 0;
-
-        response = "Per il " + date + " al docente " + toTitleCase(requestedSubstitute) + " sono assegnate le seguenti sostituzioni:\n";
-
-        for (var j = 0; j < resultingTable.length; j++) { //For each column
-          response += "\n" + beautifyArray(resultingTable[j], true, false, true, true, true);
-          numResults++;
-        }
-
-        if (numResults == 0)
-          response = "Nessuna assenza assegnata al docente " + toTitleCase(requestedSubstitute) + " per il giorno " + date + ".";
-
-        bot.sendMessage(msg.chat.id, response);
-      }
-    });
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-  });
-
-  req.write("");
-  req.end();
-
+  bot.sendMessage(msg.chat.id, response);
 }
 
 bot.onText(/^\/assenze$/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, "Specifica una classe!");
+  bot.sendMessage(msg.chat.id, "Specifica una classe!\nEsempio: /assenze CLASSE");
 });
-bot.onText(/^\/assenze\@assenzepilatibot$/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, "Specifica una classe!");
-});
-bot.onText(/\/assenze (.+)/, msgAssenze);
-bot.onText(/\/assenze\@assenzepilatibot (.+)/, msgAssenze);
 
+bot.onText(/\/assenze.* (.+)/, msgAssenze);
+
+bot.onText(/\/sostituto.* (.+)/, msgSostituto);
 bot.onText(/^\/sostituto$/, (msg, match) => {
   bot.sendMessage(msg.chat.id, "Specifica il nome!");
 });
-bot.onText(/^\/sostituto\@assenzepilatibot$/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, "Specifica il nome!");
-});
-bot.onText(/\/sostituto (.+)/, msgSostituto);
-bot.onText(/\/sostituto\@assenzepilatibot (.+)/, msgSostituto);
-bot.onText(/\/start/, (msg, match) => {
+
+bot.onText(/\/start.*/, (msg, match) => {
   bot.sendMessage(msg.chat.id, "Bot avviato. Invia un messaggio come /assenze classe");
 });
-bot.onText(/\/help/, (msg, match) => {
+bot.onText(/^\/help$/, (msg, match) => {
   bot.sendMessage(msg.chat.id, "Comandi disponibili:\n\n/assenze classe\n/sostituto nome");
 });
-bot.onText(/ping/, (msg, match) => {
+bot.onText(/^ping$/, (msg, match) => {
   bot.sendMessage(msg.chat.id, "pong");
 });
 
@@ -229,3 +127,68 @@ bot.onText(/.*/, (msg, match) => {
   });
 
 });
+
+function fixDataFormat(data) { //Fixes conceptual errors in the remote JSON
+  data.values = Array();
+
+  for (var i in data.valori) {
+    data.valori[i].id = i;
+    data.values.push(data.valori[i]);
+  }
+
+  delete data.valori; //Remove valori
+  return data;
+}
+
+function fetchData() {
+  //Fetches new data - To be called at regular intervals or after requests
+  var req = https.request(connectionOptions, function(res) {
+    var totalData = "";
+
+    res.on('data', function (data) {
+      totalData += data;
+    });
+
+    res.on('end', function () {
+      try {
+        totalData = JSON.parse(totalData);
+      } catch (e) {
+        totalData = null;
+        console.log(e instanceof SyntaxError); // true
+        console.log(e.message);                // "missing ; before statement"
+        console.log(e.name);                   // "SyntaxError"
+        console.log(e.fileName);               // "Scratchpad/1"
+        console.log(e.lineNumber);             // 1
+        console.log(e.columnNumber);           // 4
+        console.log(e.stack);                  // "@Scratchpad/1:2:3\n"
+      }
+
+      if (totalData)
+        totalData = fixDataFormat(totalData);
+
+      console.log(totalData);
+
+      if (assenze) {
+        //TODO: Fix comparison between timestamps
+        // console.log("Current last update: " + assenze.timestamp);
+        // console.log("Remote last update: " + totalData.timestamp);
+        // console.log(timediff('2015-01-01', '2018-05-02 02:15:10.777', 's'));
+        assenze = totalData;
+      } else {
+        //Do not compare
+        assenze = totalData;
+      }
+    });
+  });
+
+  req.on('error', function(e) {
+    console.log('Problem with request: ' + e.message);
+    assenze = null;
+  });
+
+  req.write("");
+  req.end();
+}
+
+fetchData(); //Fetch data immediately
+setInterval(fetchData, 5*60*1000); //Fetch data every 5 minutes
