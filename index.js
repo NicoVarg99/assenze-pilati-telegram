c35f25a053a3ba222a985ea6490ee7872089b49a
Nicola Salsotto <nicolasalsotto@gmail.com>
2018-10-02 10:09:30 +0200

    Protptype String.toTitleCase

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ var connectionOptions = {
   strictSSL: true
 };
 
-function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function(txt) {
+String.prototype.toTitleCase = function () {
+  return this.replace(/\w\S*/g, txt => {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
